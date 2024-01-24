084cdde25cb00175691de1439cda89c3e05b1bd3
MarienAngel16 <emary.presc16@gmail.com>
2024-01-23 19:27:16 -0600

    Modificar hoja de encabezado a hoja de estilos generales

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html>
+
+<html lang="en">
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -7,8 +8,10 @@
     <title>San Miguel Parroquia</title>
    
     <?php
-    include_once estable/encabezado.php;
+    include_once "estilos.php";
     ?>
+
+ </head>
 
     <main>
 
